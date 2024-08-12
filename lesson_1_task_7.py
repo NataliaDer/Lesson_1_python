ea7ce32a81260eac39fa389ea5caf03429269377
NaDe <114908293+NataliaDer@users.noreply.github.com>
2024-08-12 17:01:36 +0300

    Create lesson_1_task_7.py

---
+++ lesson_1_task_7.py
@@ -0,0 +1,47 @@
+def print_text(number):
+    print("вывести в консоль " + number)
+print_text(number = "1")
+print_text(number = "2")
+print_text(number = "3")
+print_text(number = "4")
+print_text(number = "5")
+print_text(number = "6")
+print_text(number = "7")
+print_text(number = "8")
+print_text(number = "9")
+print_text(number = "0")
+print_text=[
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0"
+]
+#88005553535
+task_number8=print_text[7]
+print(task_number8)
+task_number8=print_text[7]
+print(task_number8)
+task_number0=print_text[9]
+print(task_number0)
+task_number8=print_text[7]
+print(task_number8)
+task_number5=print_text[4]
+print(task_number5)
+task_number5=print_text[4]
+print(task_number5)
+task_number5=print_text[4]
+print(task_number5)
+task_number3=print_text[2]
+print(task_number3)
+task_number5=print_text[4]
+print(task_number5)
+task_number3=print_text[2]
+print(task_number3)
+task_number5=print_text[4]
+print(task_number5)
